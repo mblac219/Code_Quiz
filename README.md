98103dec69f864cfbdc76e54cfbee7c5671b669f
Mike Blackson <mkblcksn@gmail.com>
2022-03-10 23:42:04 -0600

    git push origin main

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Code_Quiz
+
+As a developer, i was task with developing a quiz game.  the goal was to have a start button that starts the multiple choice game and the user has to choose the correct answer during a series of questions . 
+
+
+## Screenshot of game 
+
+
+
+## Link to site
+<a href="https://github.com/mblac219/Code_Quiz">https://github.com/mblac219/Code_Quiz</a>
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-var start = document.addEventListener("start-btn")
+var start = document.addEventListener( 'start-btn' )
+startButton.addEventListener( 'click', startGame )
 
-
+//function to get game started//
 
 function startGame( ) {
+    console.log( 'started' )
+    startButton.classlist.add( 'hide' )
+
 
 }
 
@@ -12,7 +16,44 @@ function showQuestions( ) {
 
 }
 
-function chooseAnswer( ){
+function chooseAnswer( ) {
 
 
 }
+
+
+
+const gameQuestions =  [
+    { 
+        question: " What's the best way to travel? ",
+        answers:  {
+            a: "car",
+            b: "bus",
+            c: "plane",
+            d: "train"
+         },
+         correctAnswer: "c"
+    },
+    {
+        question: " Which state is more likely to have an earthquake? ",
+        answers: {
+            a: "Florida",
+            b: "Arkansas",
+            c: "Virginia",
+            d: "California"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which restaurant has a clown as a mascot?",
+        answer: {
+            a: "Chipotle",
+            b: "Burger King",
+            c: "McDonalds",
+            d: "Arby's"
+        },
+        correctAnswer: "c"           
+     },    
+    {
+
+]
